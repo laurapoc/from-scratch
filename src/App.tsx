8d0385814ad1b65013419060ed63a24a15fbb31e
laurapoc <laura.pociuniene@gmail.com>
2021-11-16 17:37:00 +0200

    fetch data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const App = () => {
+  useEffect(() => {
+    const getRepos = async () => {
+      const url = `https://api.github.com/users/laurapoc/repos?since=2020-12-01T09:07:21.20-07:00`
+      const resp = await fetch(url)
+      const body = await resp.json()
+      console.log(body)
+    }
+
+    getRepos()
+  }, [])
+
   return (
     <div>
       <h1>hello world</h1>
