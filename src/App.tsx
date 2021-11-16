87951eb6c3dc3a2be890ec76682278d6fc292555
laurapoc <laura.pociuniene@gmail.com>
2021-11-16 16:26:07 +0200

    add eslint, reset prettier default formatting rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
   return (
     <div>
-      <h1>Hi from my manual created react typescript app</h1>
+      <h1>hello world</h1>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
