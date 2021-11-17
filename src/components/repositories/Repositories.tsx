000df2017f23d74ff62ca357ad61b8b9dc5bffda
laurapoc <laura.pociuniene@gmail.com>
2021-11-17 14:28:30 +0200

    remove React Query code to output different data

--- src/components/repositories/Repositories.tsx
+++ src/components/repositories/Repositories.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query'
+import { useEffect } from 'react'
 import { useRecoilState, useRecoilValue } from 'recoil'
 
 import { repos as reposAtom, view as viewAtom } from '../../atoms'
@@ -8,21 +8,25 @@ const Repositories = () => {
   const [repos, setRepos] = useRecoilState(reposAtom)
   const view = useRecoilValue(viewAtom)
 
-  const { data, status } = useQuery('repos', () =>
-    fetch(`https://api.github.com/users/laurapoc/repos?since=${view}`).then(
-      (res) => res.json().then((data) => setRepos(data))
-    )
-  )
+  useEffect(() => {
+    const getRepos = async () => {
+      const url = `https://api.github.com/users/laurapoc/repos?since=${view}`
+      const resp = await fetch(url)
+      const body = await resp.json()
+      setRepos(body)
+    }
+
+    getRepos()
+  }, [setRepos, view])
 
   console.log(repos)
 
   return (
     <div>
-      {status === 'error'
-        ? 'Error fetching data'
-        : status === 'loading'
-        ? 'loading'
-        : repos.map((repo: any) => <Repo key={repo.id} repo={repo} />)}
+      <h4>Repositories since {view}</h4>
+      {repos.map((repo: any) => (
+        <Repo key={repo.id} repo={repo} />
+      ))}
     </div>
   )
 }
