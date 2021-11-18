f2e63eb6335a98a0668978512a71d35ed27c114b
laurapoc <laura.pociuniene@gmail.com>
2021-11-18 15:05:24 +0200

    add Repositories test

---
+++ src/components/repositories/Repositories.test.tsx
@@ -0,0 +1,42 @@
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+import Repositories from './Repositories'
+
+const queryClient = new QueryClient()
+
+const fakeRepositories = [
+  {
+    id: 1,
+    name: 'pizza_app'
+  },
+  {
+    id: 2,
+    name: 'rocket_app'
+  }
+]
+
+describe('<Repositories />', () => {
+  beforeEach(() => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Repositories />
+        </RecoilRoot>
+      </QueryClientProvider>
+    )
+  })
+
+  it('fetches repositories', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(fakeRepositories)
+      })
+    ) as jest.Mock
+
+    expect(await screen.findByText('pizza_app')).toBeInTheDocument()
+    expect(await screen.findByText('rocket_app')).toBeInTheDocument()
+  })
+})
